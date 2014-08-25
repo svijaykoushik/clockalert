5b1ca92e3ffa5b543bf0c83f088ca011f9e3358b
moon01man <moon01man@gmail.com>
2014-08-25 13:26:21 +0530

    version 1.1

---
+++ ErrorLog.cs
@@ -0,0 +1,56 @@
+﻿/***********************************************************************************
+This file is part of Clock Alert.
+
+    Clock Alert is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as published by
+    the Free Software Foundation.
+
+    Clock Alert is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Clock Alert.  If not, see <http://www.gnu.org/licenses/>.
+*************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clock_Alert
+{
+    class ErrorLog
+    {
+        /// <summary>
+        /// Parts of the log message.
+        /// </summary>
+        static string header, message, footer, path, folderpath;
+
+        /// <summary>
+        /// Logs the error or exception to a file.
+        /// </summary>
+        /// <param name="ex">The Exception object of the Exception thrown for logging.</param>
+        /// <returns></returns>
+        public static string logError(Exception ex)
+        {
+            header = DateTime.Now.ToString() + "\n\n";
+            message = ex.Message;
+            footer = "\n\n\n";
+            folderpath =Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            path=folderpath+ @"\ClockALert Log" + DateTime.Now.ToShortDateString()+".txt";
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(path, true);
+            try
+            {
+                sw.WriteLine(header);
+                sw.WriteLine(message);
+                sw.WriteLine(footer);
+                sw.Close();
+                return path;
+            }
+            catch (Exception exp)
+            {
+                return exp.ToString();
+            }
+        }
+    }
+}
---
+++ ICrashReporter.cs
@@ -0,0 +1,31 @@
+﻿/***********************************************************************************
+This file is part of Clock Alert.
+
+    Clock Alert is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as published by
+    the Free Software Foundation.
+
+    Clock Alert is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Clock Alert.  If not, see <http://www.gnu.org/licenses/>.
+*************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clock_Alert
+{
+    /// <summary>
+    /// Contains methods to report crashes to developer
+    /// </summary>
+    interface ICrashReporter
+    {
+        void getException(Exception exception);
+        void createReport();
+        void sendReport();
+    }
+}
