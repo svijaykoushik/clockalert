7181aa5cd9cfe4df509f95047ce7b8c5559d67ae
Vijay Koushik, S <moon01man@users.sourceforge.net>
2018-12-08 23:23:02 +0530

    Fixed: App fails to restart after changing settings bug
    The bug was fixed by changing to the Mutex method of making the app
    Single Instance only app instead of searching the running processes to
    see if the app's process was running already.
    The later method prevented the app from restarting because, when the app
    attempts to restart the app is still in the running processes list and
    thus prevents the app from restarting.

--- Program.cs
+++ Program.cs
@@ -15,18 +15,63 @@ This file is part of Clock Alert.
 *************************************************************************************/
 using System;
 using System.Windows.Forms;
-using System.Diagnostics;
+using System.Threading;
+//using System.Diagnostics;
 
 namespace Clock_Alert
 {
     static class Program
     {
         /// <summary>
+        /// The named mutual exclusion object that is
+        /// owned by the first instance of the app that is created.
+        /// It holds the first application instance (process) 
+        /// and prevents the app from running in a new instace (process).
+        /// </summary>
+        static Mutex mutExcl;
+
+        /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            bool firstInstance = false;
+            /*
+             * New mutual exclusion object is created with
+             * the current thread as owner.
+             * It is given the same name as the app.
+             * If the object was succesfully created with the current thread as owner,
+             * firstInstance variable is set to true.
+             */
+            mutExcl = new Mutex(true, Application.ProductName.ToString(), out firstInstance);
+            /*
+             * If the Mutual exclusion object was succesfully created
+             * then we start the application
+             * If not
+             * It means that the mutex object is owned by the first instance
+             * and a second instance trying to start the application.
+             * So we show a message that the application is already running.
+             */ 
+            if(firstInstance)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                using (Startup app = new Startup())
+                {
+                    app.startApp();
+                    Application.Run();
+                }
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Clock Alert is already running!", "Clock Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
+            /*
+             * Comenting this code because it sometimes prevent
+             * the app from restarting when the settings are
+             * changed by the user. and the app closes.
             Process currentProcess = Process.GetCurrentProcess();
             Process[] runningProcess = Process.GetProcessesByName(currentProcess.ProcessName);
             int numberOfProcess = runningProcess.Length;
@@ -45,7 +90,7 @@ namespace Clock_Alert
             else
             {
                 System.Windows.Forms.MessageBox.Show("I'm here already!", "Clock Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
+            }*/
         }
 
         static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
